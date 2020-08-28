4c101642bd2337e70c09aaaab5f1d5a9bfc41cce
RebeccaChavezLV <rebeccachavezlv@gmail.com>
2020-08-28 10:32:21 -0700

    converted to async function, working on styling

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//CORS Anywhere
+const proxyUrl = 'https://protected-lowlands-50643.herokuapp.com/'
+
+//Leaflet Map Initialize
 let mymap = L.map('mapid').setView([36.1699, 115.1398], 2);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,59 +12,75 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoicmViZWNjYWNoYXZlemx2IiwiYSI6ImNrZTRlaDRzYzBzYXQyc21xYWViamF4d2YifQ.2kGwr_kzrmAD7USV7ajfeA'        
 }).addTo(mymap);
 
+//Leaflet Map Icon
 let myIcon = L.icon({
     iconUrl: 'spaceship.png',
     iconSize: [35, 35],
 });
 
+//If higher than one, marker will update instead of initialize
 let counter = 0;
 
+//Default starting point for marker. Hidden until map is initialized
 let marker = L.marker([36.1699, 115.1398],{icon: myIcon})
 
-const latLong = (result) => {
-    document.getElementById('placeCoords').innerHTML = 'Latitude: ' + result.location.latitude + '<br>Longitude: ' + result.location.longitude;
-}
-
-const astros = (result) => {
-    const peopleArray = result.astros;
-    document.getElementById("placeAstros").innerHTML = "";
-    for (i = 0; i < peopleArray.length; i++){
-        const li = document.createElement("li");
-        let textNode = document.createTextNode(peopleArray[i].name);
-        li.appendChild(textNode);
-        document.getElementById("placeAstros").appendChild(li);   
+//Finds location, places it on map, and updates text on page
+async function findLatLong () {
+    const apiUrl = 'http://api.open-notify.org/iss-now.json'
+    try {
+        const response = await fetch (proxyUrl + apiUrl);
+        const data = await response.json();
+        // Places coordinates in text
+        document.getElementById('placeCoords').innerHTML = 'Latitude: ' + data.iss_position.latitude + '<br>Longitude: ' + data.iss_position.longitude;
+        //Initializes marker on map
+        if (counter === 0){
+            counter++;
+            mymap.flyTo([data.iss_position.latitude, data.iss_position.longitude], 2);
+            marker.setLatLng([data.iss_position.latitude, data.iss_position.longitude]).update();
+            marker.addTo(mymap);
+            console.log('Initializing')
+        } else {
+            // Updates marker on map
+            mymap.flyTo([data.iss_position.latitude, data.iss_position.longitude], 2);
+            marker.setLatLng([data.iss_position.latitude, data.iss_position.longitude]).update();
+            console.log('Updating')
+        }
+        return data
+    } catch (error) {
+        console.log (error)
     }
 }
 
-const initializeMarker = (result) => {
-    mymap.flyTo([result.location.latitude, result.location.longitude], 2);
-    marker.setLatLng([result.location.latitude, result.location.longitude]).update();
-    marker.addTo(mymap);
-    console.log('initializing marker')
+//Finds astronauts and updates text on page
+async function astroInfo() {
+    const apiUrl = 'http://api.open-notify.org/astros.json'
+    try {
+        const reponse = await fetch (proxyUrl + apiUrl);
+        const data = await reponse.json();
+        const peopleArray = data.people;
+        //Places astronauts in text
+        document.getElementById("placeAstros").innerHTML = '';
+        for (i = 0; i < peopleArray.length; i++){
+            const li = document.createElement("li");
+            let textNode = document.createTextNode(peopleArray[i].name);
+            li.appendChild(textNode);
+            document.getElementById("placeAstros").appendChild(li);   
+        }
+     
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-const updateMarker = (result) => {
-    mymap.flyTo([result.location.latitude, result.location.longitude], 2);
-    marker.setLatLng([result.location.latitude, result.location.longitude]).update();
-    console.log('updating marker')
-}
+//Click event
 
 const issInfo = () => {
-    fetch('https://lit-temple-91342.herokuapp.com/')
-        .then(response => response.json())
-        .then(result => {
-            latLong(result);
-            astros(result);
-            if (counter === 0){
-                counter++;
-                initializeMarker(result)
-            } else {
-                updateMarker(result);
-            }
-        }
-    )
+    findLatLong();
+    astroInfo();
 }
 
+//Event Listeners
+    
 locationButton.addEventListener("click", () => {
     issInfo();
 })
